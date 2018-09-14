bf9a27b66fb77a3fbdef6ffe9763718028aabc49
Ivani Aviles-Carbajal <ivaniaviles@gmail.com>
2018-09-13 20:07:41 -0500

    attempting to clear up log error

--- index.js
+++ index.js
@@ -35,5 +35,13 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/submissionRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
